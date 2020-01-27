e7a4be15a54af5bca69ef64587a8f8e714cda472
Juan Camilo Dvera <jdvera@easysol.net>
2020-01-27 15:51:11 -0500

    se agregan primeros escenarios

---
+++ cypress/integration/examples/test.js
@@ -0,0 +1,35 @@
+/// <reference types="Cypress" />
+
+describe('My First Test', function() {
+    it('My First TestCase', function() {
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000);
+        //cy.get('.product:visible').should('have.length',4);
+        //Parent-child chaining
+        cy.get('.products').find('.product').should('have.length',4);
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
+    })
+    it('My Second TestCase', function() {
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000);
+        cy.get('.product:visible').should('have.length',4);
+    })
+    it('Search product by name', function() {
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000);
+        cy.get('.products').find('.product').should('have.length',4);
+
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            const productName = $el.find('h4.product-name').text();
+            if(productName.includes('Cashews')) {
+                $el.find('button[type=button]').click();
+            }
+        })
+    })
+    it('Logo', function() {
+        const logo = cy.get()
+    })
+})
