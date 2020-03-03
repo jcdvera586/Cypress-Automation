4db3b8ef057901ba4cda9fd54e157830bd7ff4d6
Juan Camilo Dvera <jdvera@easysol.net>
2020-03-02 20:18:15 -0500

    prueba fixtures con hooks

--- cypress/integration/examples/testFramework.js
+++ cypress/integration/examples/testFramework.js
@@ -1,13 +1,19 @@
 /// <reference types="Cypress" />
 
 describe('Framework test cases', function() {
-    before(function() {
 
+    before(function() {
+        cy.fixture('example').then(function(data) {
+            this.data = data
+        })
     })
+
     it('first TestFramework testcase', function() {
         cy.visit('https://www.rahulshettyacademy.com/angularpractice/')
-        cy.get('input[name="name"]:nth-child(2)').type("Bob")
-        cy.get('select').select("Female")
-
+        cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
+        cy.get('select').select(this.data.gender)
+        cy.get('input[name="name"]:nth-child(1)').should('have.value', this.data.name)
+        cy.get('input[name="name"]:nth-child(2)').should('have.attr', 'minlength', '2')
+        cy.get('#inlineRadio3').should('be.disabled')
     })
 })
